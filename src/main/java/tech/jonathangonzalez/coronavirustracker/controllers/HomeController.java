4fcce10ae3e0b0d35a42c102a470c549a9916870
Gjonathan252 <g.jonathan252@gmail.com>
2022-02-13 01:12:26 -0800

    Added /global data + fixed footer

--- src/main/java/tech/jonathangonzalez/coronavirustracker/controllers/HomeController.java
+++ src/main/java/tech/jonathangonzalez/coronavirustracker/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package tech.jonathangonzalez.coronavirustracker.controllers;
 
+import tech.jonathangonzalez.coronavirustracker.models.Global_LocationStats;
 import tech.jonathangonzalez.coronavirustracker.models.LocationStats;
 import tech.jonathangonzalez.coronavirustracker.services.CoronaVirusDataService;
+import tech.jonathangonzalez.coronavirustracker.services.Global_CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,5 +27,20 @@ public class HomeController {
         model.addAttribute("totalNewCases", totalNewCases);
 
         return "index";
+    }
+
+    @Autowired
+    Global_CoronaVirusDataService Global_CoronaVirusDataService;
+
+    @GetMapping("/global")
+    public String global_VirusData(Model model) {
+        List<Global_LocationStats> allStats = Global_CoronaVirusDataService.getAllGlobalStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("Global_locationStats", allStats);
+        model.addAttribute("Global_totalReportedCases", totalReportedCases);
+        model.addAttribute("Global_totalNewCases", totalNewCases);
+
+        return "global";
     }
 }
